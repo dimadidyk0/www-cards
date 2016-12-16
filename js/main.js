88349ab3c53a66685ed4576ca52eb5217ffda6f7
Dima <dimadidyk0@gmail.com>
2016-12-16 14:43:09 +0200

    changed fonts, files

--- js/main.js
+++
@@ -1,9 +0,0 @@
-
-   function changeRangeValue() {
-    var rangeValue = document.getElementById("other-range-size").value;
-    var textValue = document.getElementById("range-value");
-    textValue.innerHTML = rangeValue;
-    var rangeTextIndent = 8 + 10*rangeValue;
-	document.getElementById("range-text").style.left = String(rangeTextIndent)+"%";
-   }
-  	
